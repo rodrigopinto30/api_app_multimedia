3624fa10494dd18e8d2c66f62013a6392c115b14
rorro <rodrigopinto5050@gmail.com>
2021-11-19 11:04:17 -0300

    19/11

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'src/.env' });
+const userRoutes = require('./routes/user');
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+// Middleware
+app.use(express.json());
+app.use('/api', userRoutes);
+
+// Routes
+app.get('/', (req, res)=>{
+    res.send('Welcome to my page');
+});
+
+// Mongo db connection
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(()=>console.log('Connected to MongoDB Atlas'))
+    .catch((error)=>console.error(error));
+
+
+
+app.listen(port, ()=>console.log('server listening on port', port));
